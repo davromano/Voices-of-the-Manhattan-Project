0715c035d1d9febbef8e4a21a53be529f27e3c44
davromano <davidesmail00@gmail.com>
2023-04-03 20:57:33 +0200

    Scraped all interviews, format output to change

---
+++ scraper_main.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from page_scraper import page_scraper
+
+def main():
+    main_url = 'https://ahf.nuclearmuseum.org/voices/oral-histories/'
+    results = []
+
+    # Loop through pages 1 to 60
+    for page in range(1, 61):
+        if page == 1:
+            current_url = main_url
+        else:
+            current_url = f'{main_url}?_paged={page}'
+        
+        main_response = requests.get(current_url)
+        main_soup = BeautifulSoup(main_response.content, 'html.parser')
+
+        post_list = main_soup.find('div', {'class': 'post-list'})
+        post_list_items = post_list.find_all('div', {'class': 'post-list-item'})
+
+        urls = [item.find('a')['href'] for item in post_list_items]
+
+        for url in urls:
+            result = page_scraper(url)
+            results.append(result)
+        print(page)
+
+if __name__ == "__main__":
+    main()
